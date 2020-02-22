9fe42ca6d04e15cb836cc8befa8758fc658f0325
FreeMcPremiumAccounts <61262257+FreeMcPremiumAccounts@users.noreply.github.com>
2020-02-21 22:27:15 -0700

    bruh

--- App.js
+++ App.js
@@ -2,4 +2,11 @@ var t = document.createElement("h1");
 t.innerHTML = "Free Minecrft Alts";
 t.style.color="lime"
 document.body.append(t);
+
 document.body.style.backgroundColor = "orange";
+
+var des = document.createElement("h2");
+des.innerHTML = "Download the File from Media Fire below:"
+des.style.position = "absolute";
+des.style.top = "50px";
+document.body.append(des);
