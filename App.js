a888f7b1b4a0c47dac175605b1e500aec9518e83
FreeMcPremiumAccounts <61262257+FreeMcPremiumAccounts@users.noreply.github.com>
2020-02-21 22:22:05 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,4 +2,4 @@ var t = document.createElement("h1");
 t.innerHTML = "free minecrft alts";
 t.style.color="purple"
 document.body.append(t);
-document.body.style.backgroundColor = "pink";
+document.body.style.backgroundColor = "orange";
